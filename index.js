7657412a3026797154964cea1bb6f05c332e70a7
Michael Webb <mjwebb1993@yahoo.com>
2024-05-14 21:45:39 -0500

    Step 12.iii

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { header, nav, main, footer } from "./components";
+
+function render() {
+  document.querySelector("#root").innerHTML = `
+      ${header()}
+      ${nav()}
+      ${main()}
+      ${footer()}
+    `;
+  afterRender();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
+render();
